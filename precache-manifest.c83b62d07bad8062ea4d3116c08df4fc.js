69103c5a3ba1bef480d0de1b4613485d766013d1
ManuCas <manucastle@hotmail.com>
2019-07-12 17:58:54 -0500

    Updates

---
+++ precache-manifest.c83b62d07bad8062ea4d3116c08df4fc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23704b13fa46a697d39c16b4ac4c6aa",
+    "url": "/clicky-react/index.html"
+  },
+  {
+    "revision": "9710f81427db681cac13",
+    "url": "/clicky-react/static/js/2.1d9c299a.chunk.js"
+  },
+  {
+    "revision": "8d1d3cb995800a695d56",
+    "url": "/clicky-react/static/js/main.ffc946fe.chunk.js"
+  },
+  {
+    "revision": "715effbbbe4788c7de9c",
+    "url": "/clicky-react/static/js/runtime~main.e9ac801e.js"
+  }
+]);
